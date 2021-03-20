77d7cf7fc7ee6ea1e53ace8a5347c6f069e9cdea
Alex Petryanin <aspetryanin@gmail.com>
2021-03-20 16:44:33 +0200

    Added docker-compose

--- db.inc.php
+++ db.inc.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 define('DB_LOGIN', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'secret');
 define('DB_NAME', 'project');
 
 $link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
